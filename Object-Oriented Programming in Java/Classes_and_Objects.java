2a20d5e589fb512ee6ac3c5552f3b712752976b1
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-03 20:26:09 -0300

    Variables

--- Object-Oriented Programming in Java/Classes_and_Objects.java
+++ Object-Oriented Programming in Java/Classes_and_Objects.java
@@ -1,13 +1,21 @@
-class Variables {
-    public static void main(String[] args) {
+// class Variables {
+//     public static void main(String[] args) {
 
-        String output = "Hi Ann!";
-        System.out.println(output);
+//         String output = "Hi Ann!";
+//         System.out.println(output);
         
-        output = "Hi Ralf!";
-        System.out.println(output);
+//         output = "Hi Ralf!";
+//         System.out.println(output);
 
-    }
-}
+//     }
+// }
 
 // <data_type>   <identifier> =   <value>;
+
+class Hello {
+    public static void main(String[] args){
+        int number1 = 5;
+        int number2 = 5;
+        System.out.println(number1 + number2);
+    }
+}
