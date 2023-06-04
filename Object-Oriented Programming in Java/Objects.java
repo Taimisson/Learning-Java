dc9eacf3878a5eded9c00dd70db7243dbd4c25d9
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-04 00:26:54 -0300

    Object and Method

--- Object-Oriented Programming in Java/Objects.java
+++ Object-Oriented Programming in Java/Objects.java
@@ -25,35 +25,49 @@
 //     }
 // }
 
-class Detective{
-    void speak(){
-        System.out.println("Hello");
+// class Detective{
+//     void speak(){
+//         System.out.println("Hello");
+//     }
+// }
+
+// class MethodCall{
+//     public static void main(String[] args){
+//         Detective duke = new Detective();
+//         Detective ann = new Detective();
+//         duke.speak();
+//         ann.speak();
+//     }
+// }
+
+// class MethodCalls{
+//     void initial(){
+//         first();
+//         second();
+//         System.out.println("Last");
+//     }
+//     void second(){
+//         System.out.println("Second");
+//         third();
+//     }
+//     void first(){
+//         System.out.println("First");
+//     }
+//     void third(){
+//         System.out.println("Third");
+//     }
+// }
+
+class Detective {
+    public void sayYourProfession() {
+        System.out.println("Detective");
     }
 }
 
-class MethodCall{
-    public static void main(String[] args){
+class Story {
+    public static void main(String[] args) {
         Detective duke = new Detective();
-        Detective ann = new Detective();
-        duke.speak();
-        ann.speak();
+        duke.sayYourProfession();
     }
 }
 
-class MethodCalls{
-    void initial(){
-        first();
-        second();
-        System.out.println("Last");
-    }
-    void second(){
-        System.out.println("Second");
-        third();
-    }
-    void first(){
-        System.out.println("First");
-    }
-    void third(){
-        System.out.println("Third");
-    }
-}
