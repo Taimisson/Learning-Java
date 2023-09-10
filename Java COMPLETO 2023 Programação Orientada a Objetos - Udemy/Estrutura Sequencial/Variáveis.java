bdea5888e3e172fc6c24949d6c8f7bac915534bf
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-09-09 22:13:08 -0300

    Casting e Processamento

---
+++ Java COMPLETO 2023 Programação Orientada a Objetos - Udemy/Estrutura Sequencial/Variáveis.java
@@ -0,0 +1,40 @@
+public class Variable {
+
+    public static void main(String[] args){
+
+        // Exemplo 1
+//        int x;
+//        double y;
+//
+//        x = 5;y = 2 * x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+        // Exemplo 2
+
+//        double b, B, h, area;
+//
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//
+//        area = (b + B) / 2.0 * h;
+//
+//        System.out.println(area);
+
+        // Exemplo 3
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; // CASTING é a conversão explícita dos valores
+
+        System.out.println(resultado);
+    }
+
+
+}
