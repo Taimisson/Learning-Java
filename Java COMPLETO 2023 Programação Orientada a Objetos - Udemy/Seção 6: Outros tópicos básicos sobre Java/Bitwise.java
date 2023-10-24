f004a3b0f7ea85110e4c07169a6d9b6f1702f060
Taimisson C <100889845+Taimisson@users.noreply.github.com>
2023-10-24 00:48:37 -0300

    Basic Java

---
+++ Java COMPLETO 2023 Programação Orientada a Objetos - Udemy/Seção 6: Outros tópicos básicos sobre Java/Bitwise.java
@@ -0,0 +1,16 @@
+package OutrosTopicosBasicos;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        // & - Operação E bit a bit.
+        // | - Operação OU bit a bit
+        // ^ - Operação OU-Exclusivo bit a bit
+
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+    }
+}
---
+++ Java COMPLETO 2023 Programação Orientada a Objetos - Udemy/Seção 6: Outros tópicos básicos sobre Java/Restrictions.java
@@ -0,0 +1,22 @@
+package OutrosTopicosBasicos;
+
+public class Restrictions {
+    public static void main(String[] args) {
+//         Restricões para nomes de variáveis
+//         Nao pode começar com digito: use uma letra ou _
+//         Nao pode acentos ou til
+//         Nao pode ter espaço em branco
+//         Sugestão:  use nomes que tenham um significado
+
+        // Convenções
+//        Camel Case: lastName
+//                * pacotes
+//                * atributos
+//                * métodos
+//                * variáveis e parâmetros
+//        Pascal Case: ProductService
+//                * classes
+
+    }
+
+}
